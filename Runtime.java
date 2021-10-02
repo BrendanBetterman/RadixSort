2ccd16f55aadb753138c960f5dffb537c37eb754
BrendanBetterman <58009876+BrendanBetterman@users.noreply.github.com>
2021-10-01 22:54:27 -0500

    Update Runtime.java
    condenced

--- Runtime.java
+++ Runtime.java
@@ -2,26 +2,25 @@ package RadixSort;
 import java.util.Random;
 
 public class Runtime {
+    public static void printList(int[] tmpList){
+        for(int i = 0; i<tmpList.length; i++){
+                System.out.println(tmpList[i]);
+            }
+    }
     public static void main(String[] args){
         int[] powTen = new int[]{10};
         Random RandGen = new Random();
         Radix sort = new Radix();
-       
         int max = 999;
         int min = 100;
-        for (int u = 0; u<times; u ++){ 
-            for(int i =0; i<list.length; i++){
-                list[i] = RandGen.nextInt(max - min) + min;
-            }
-            for(int i = 0; i<list.length; i++){
-                System.out.println(list[i]);
-            }
-            System.out.println("sorted");
-            sort.in(list);
-            list = sort.getSorted();
-            for(int i = 0; i<list.length; i++){
-                System.out.println(list[i]);
-            }  
+     
+        for(int i =0; i<list.length; i++){
+            list[i] = RandGen.nextInt(max - min) + min;
         }
+        printList(list);
+        System.out.println("sorted");
+        sort.in(list);
+        list = sort.getSorted();
+        printList(list);  
     }   
 }
